33ec17dff178a2fdd082e88ed2baf9256bed8988
CLEMDEF <clementdefombelle@gmail.com>
2023-11-18 18:17:07 -0500

    debut http pour random

--- source/Random.hpp
+++ source/Random.hpp
@@ -2,15 +2,103 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <curl/curl.h>
+#include <stdio.h>
+#include <iostream>
+
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if(mem->memory == NULL) {
+    /* out of memory! */ 
+    std::cout << "not enough memory (realloc returned NULL)" << std::endl;
+    return 0;
+  }
+
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+
+  return realsize;
+}
 
 class Random
 {
 private:
     static Random instance;
+    std::string Answer;
 
 public:
     Random()
     {
+        CURL *curl_handle;
+        CURLcode res;
+
+        struct MemoryStruct chunk;
+
+        chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+        chunk.size = 0;    /* no data at this point */ 
+
+        curl_global_init(CURL_GLOBAL_ALL);
+
+        /* init the curl session */ 
+        curl_handle = curl_easy_init();
+
+        /* specify URL to get */ 
+        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint16&size=6");
+
+        /* send all data to this function  */ 
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+        /* we pass our 'chunk' struct to the callback function */ 
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+        /* some servers don't like requests that are made without a user-agent
+            field, so we provide one */ 
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+        /* get it! */ 
+        res = curl_easy_perform(curl_handle);
+
+        /* check for errors */ 
+        if(res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+        else
+        {
+            /*
+            * Now, our chunk.memory points to a memory block that is chunk.size
+            * bytes big and contains the remote file.
+            *
+            * Do something nice with it!
+            */ 
+            Answer = chunk.memory;
+            pos = Answer.find("")
+            seed = Answer.substr(0,)
+            std::cout << Answer << std::endl;
+            std::cout << (long)chunk.size << "bytes retrieved" << std::endl;
+        }
+
+        /* cleanup curl stuff */ 
+        curl_easy_cleanup(curl_handle);
+
+        if(chunk.memory)
+            free(chunk.memory);
+
+        /* we're done with libcurl, so clean it up */ 
+        curl_global_cleanup();
         srand(time(0));
     }
 

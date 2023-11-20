b3832987595dea6c0cde5ead227477c862f6e778
Clément Lebeau <clementlebeau13@gmail.com>
2023-11-20 13:04:37 -0500

    Updated build script.

--- source/build.sh
+++ source/build.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-g++ -ggdb `pkg-config --cflags SDL2_ttf SDL2 SDL2_image SDL2_mixer` -o program.exe main.cpp `pkg-config --libs SDL2_ttf SDL2 SDL2_image SDL2_mixer`
+g++ -ggdb `pkg-config --cflags --libs SDL2_ttf SDL2 SDL2_image SDL2_mixer openssl` -lcurl.dll -o program.exe main.cpp
